ce7c1386b5027b2ebd58a2df622e5ea6f46156a8
Anatoli Nikolaevich <tolya.tar@gmail.com>
2022-10-02 18:24:13 +0300

    some elements added

--- src/components/basicAlert/basicAlert.tsx
+++ src/components/basicAlert/basicAlert.tsx
@@ -3,6 +3,7 @@ import Alert from '@mui/material/Alert';
 import Stack from '@mui/material/Stack';
 import {useAppDispatch} from "../../store/store";
 import {setErrorValueAC} from "../../store/app-reducer";
+import {useEffect} from "react";
 
 export function BasicAlert() {
 
@@ -16,6 +17,9 @@ export function BasicAlert() {
     //         dispatch(setErrorValueAC(false))
     //     }, 3000)
     // }
+    useEffect(() => {
+        setTimeout(() => { onCloseError()}, 2000)
+    }, [])
     return (
         <Stack sx={{width: '100%'}} spacing={2}>
             <Alert severity="error" onClose={onCloseError} >please, type only numbers...</Alert>
