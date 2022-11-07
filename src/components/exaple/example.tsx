46153f285d003fda42a04dce920856bfbca3a8f4
Anatoli Nikolaevich <tolya.tar@gmail.com>
2022-11-07 19:17:04 +0300

    example fixed

--- src/components/exaple/example.tsx
+++ src/components/exaple/example.tsx
@@ -6,7 +6,6 @@ export const Example = () => {
             <h4>Example:</h4>
             <p>Minsk = lat: 53.9, lon: 27.5667</p>
             <p>Mozyr = lat: 52.0495, lon: 29.2456</p>
-            <p>New York = lat: 40.730610, lon: -73.935242</p>
         </div>
     );
 };
